d91a5100e9c63265d0ad947e84003a9ba59f8f34
Seb Glazebrook <me@sebglazebrook.com>
2014-04-09 21:37:03 +1000

    Gem template

---
+++ lib/phonegap_build/client.rb
@@ -0,0 +1,7 @@
+require "phonegap_build/client/version"
+
+module PhonegapBuild
+  module Client
+    # Your code goes here...
+  end
+end
---
+++ lib/phonegap_build/client/version.rb
@@ -0,0 +1,5 @@
+module PhonegapBuild
+  module Client
+    VERSION = "0.0.1"
+  end
+end

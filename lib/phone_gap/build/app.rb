30c8fe9e830af79050c830b125649b2aa3440c2c
Seb Glazebrook <me@sebglazebrook.com>
2014-04-16 19:25:43 +1000

    Adding missing attr_accessors

--- lib/phone_gap/build/app.rb
+++ lib/phone_gap/build/app.rb
@@ -5,6 +5,9 @@ module PhoneGap
   module Build
     class App < RestResource
 
+      attr_accessor :title, :create_method, :package, :version, :description, :debug, :keys, :private,
+                    :phonegap_version, :hydrates
+
       PATH = '/apps'
       attr_creatable :title, :create_method, :package, :version, :description, :debug, :keys, :private, :phonegap_version, :hydrates
       attr_updatable :title, :package, :version, :description, :debug, :private, :phonegap_version
